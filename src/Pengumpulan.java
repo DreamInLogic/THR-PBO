71bbb41576942d46f25a9aca33d704ff9631e6a9
DreamInLogic <sunusetyojati@gmail.com>
2026-03-16 15:55:26 +0700

    first commit

--- src/Pengumpulan.java
+++ src/Pengumpulan.java
@@ -5,29 +5,29 @@ private Tugas tugas;
 private String tanggalKumpul;
 private String status;
 
-public Pengumpulan(Mahasiswa mahasiswa, Tugas tugas, String tanggalKumpul) {
+    public Pengumpulan(Mahasiswa mahasiswa, Tugas tugas, String tanggalKumpul) {
 
-    this.mahasiswa = mahasiswa;
-    this.tugas = tugas;
-    this.tanggalKumpul = tanggalKumpul;
+        this.mahasiswa = mahasiswa;
+        this.tugas = tugas;
+        this.tanggalKumpul = tanggalKumpul;
 
-    if (tanggalKumpul.compareTo(tugas.getDeadline()) <= 0) {
-        status = "Tepat Waktu";
-    } else {
-        status = "Terlambat";
+        if (tanggalKumpul.compareTo(tugas.getDeadline()) <= 0) {
+            status = "Tepat Waktu";
+        } else {
+            status = "Terlambat";
+        }
+
     }
 
-}
+    public void tampilkan() {
 
-public void tampilkan() {
-
-    System.out.println("Mahasiswa : " + mahasiswa.getNama());
-    System.out.println("Tugas     : " + tugas.getJudul());
-    System.out.println("Deadline  : " + tugas.getDeadline());
-    System.out.println("Tanggal   : " + tanggalKumpul);
-    System.out.println("Status    : " + status);
-    System.out.println("==========================");
-}
+        System.out.println("Mahasiswa : " + mahasiswa.getNama());
+        System.out.println("Tugas     : " + tugas.getJudul());
+        System.out.println("Deadline  : " + tugas.getDeadline());
+        System.out.println("Tanggal   : " + tanggalKumpul);
+        System.out.println("Status    : " + status);
+        System.out.println("==========================");
+    }
 
     public Mahasiswa getMahasiswa() {
         return mahasiswa;
